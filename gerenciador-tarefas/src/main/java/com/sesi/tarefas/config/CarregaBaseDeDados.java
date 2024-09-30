3bbaceb42df17526c91abb72778302ca43831854
kahuanPR <158178600+kahuanPR@users.noreply.github.com>
2024-09-30 11:49:05 -0300

    Add files via upload

---
+++ gerenciador-tarefas/src/main/java/com/sesi/tarefas/config/CarregaBaseDeDados.java
@@ -0,0 +1,30 @@
+package com.sesi.tarefas.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.sesi.tarefas.model.TarefaCategoria;
+import com.sesi.tarefas.repository.TarefaCategoriaRepository;
+
+@Configuration
+public class CarregaBaseDeDados {
+	@Autowired
+	private TarefaCategoriaRepository tarefaCategoriaRepository;
+	
+	@Bean
+	CommandLineRunner executar() {
+		return args ->{
+			TarefaCategoria categoria = new TarefaCategoria();
+			categoria.setNome("estudos");
+			
+			tarefaCategoriaRepository.save(categoria);
+			
+			TarefaCategoria categoria1 = new TarefaCategoria();
+			categoria1.setNome("Pesquisa");
+			tarefaCategoriaRepository.save(categoria1);
+		};
+	}
+
+}
